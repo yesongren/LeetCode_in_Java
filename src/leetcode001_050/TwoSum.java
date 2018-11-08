92c1a7a477f550c92f345f5eed877ba7b5ecca3c
yesongren <youshouninn@gmail.com>
2018-11-08 14:46:16 +0800

    Add annotation

--- src/leetcode001_050/TwoSum.java
+++ src/leetcode001_050/TwoSum.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class TwoSum {
 
-    /** #001
+    /** #001 Two Sum
      * Given nums = [2, 7, 11, 15], target = 9,
      *
      * Because nums[0] + nums[1] = 2 + 7 = 9,
@@ -13,24 +13,26 @@ public class TwoSum {
      *
      * 用key装值，用value装index
      */
-    public int[] twoSum(int[] numbers, int target) {
-        if (numbers == null || numbers.length < 2) return new int[] {-1, -1};
-        int[] res = new int[] {-1, -1};
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i < numbers.length; i++) {
-            if (map.containsKey(target - numbers[i])) {
-                res[0] = map.get(Math.min(target - numbers[i], i));
-                res[1] = map.get(Math.max(target - numbers[i], i));
-            }
-            map.put(numbers[i], i);
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[] {-1, -1};
         }
-        return res;
+        int[] result = new int[] {-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] array = {0,2,4,6,7,13};
+        int[] array = {0,2,4,6,7,11};
         TwoSum twoSum = new TwoSum();
         //System.out.println(twoSum.twoSum(array, 13));    //这样输出的是数组的地址
-        System.out.println(Arrays.toString(twoSum.twoSum(array, 13)));
+        System.out.println(Arrays.toString(twoSum.twoSum(array, 17)));
     }
 }
