b3de1dc6e498a648d3676cec0f07416c7e12c744
yesongren <youshouninn@gmail.com>
2018-11-20 01:06:57 +0800

    Accepted solutions #001

--- src/leetcode001_050/TwoSum.java
+++ src/leetcode001_050/TwoSum.java
@@ -3,15 +3,20 @@ package leetcode001_050;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/**
+ * Created by yesongren on 2018/11/19
+ */
 public class TwoSum {
 
     /** #001 Two Sum
-     * Given nums = [2, 7, 11, 15], target = 9,
+     * Description:
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
      *
+     * Example:
+     * Given nums = [2, 7, 11, 15], target = 9,
      * Because nums[0] + nums[1] = 2 + 7 = 9,
      * return [0, 1].
-     *
-     * 用key装值，用value装index
      */
     public int[] twoSum(int[] nums, int target) {
         if (nums == null || nums.length < 2) {
