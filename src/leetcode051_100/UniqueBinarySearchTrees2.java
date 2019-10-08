a6cad0bd2375aa3696e2d488969ad75c6ab4ea93
yesongren <youshouninn@gmail.com>
2019-10-09 03:16:48 +0800

    accepted solutions #095

---
+++ src/leetcode051_100/UniqueBinarySearchTrees2.java
@@ -0,0 +1,77 @@
+package leetcode051_100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yesongren on 2019/10/8
+ */
+public class UniqueBinarySearchTrees2 {
+    public List<TreeNode> generateTrees(int n) {
+
+        return genTrees(1, n);
+    }
+
+    public List<TreeNode> genTrees(int start, int end) {
+
+        List<TreeNode> list = new ArrayList<TreeNode>();
+
+        if (start > end) {
+            list.add(null);
+            return list;
+        }
+
+        if (start == end) {
+            list.add(new TreeNode(start));
+            return list;
+        }
+
+        List<TreeNode> left, right;
+        for (int i = start; i <= end; i++) {
+
+            left = genTrees(start, i - 1);
+            right = genTrees(i + 1, end);
+
+            for (TreeNode lnode : left) {
+                for (TreeNode rnode : right) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = lnode;
+                    root.right = rnode;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+
+    /************* Solution 2 *************/
+
+    public List<TreeNode> generateTrees2(int n) {
+        return genTreeList(1, n);
+    }
+
+    private List<TreeNode> genTreeList(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
+            list.add(null);
+        }
+        for (int idx = start; idx <= end; idx++) {
+            List<TreeNode> leftList = genTreeList(start, idx - 1);
+            List<TreeNode> rightList = genTreeList(idx + 1, end);
+            for (TreeNode left : leftList) {
+                for (TreeNode right : rightList) {
+                    TreeNode root = new TreeNode(idx);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        UniqueBinarySearchTrees2 obj = new UniqueBinarySearchTrees2();
+        obj.generateTrees(5);
+    }
+}
