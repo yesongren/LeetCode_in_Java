ec3e068c0cd4c619251eca7417f12977d506df6d
yesongren <youshouninn@gmail.com>
2019-10-11 16:37:09 +0800

    accepted solutions #098, add inorder traversal sol

--- src/leetcode051_100/ValidateBinarySearchTree.java
+++ src/leetcode051_100/ValidateBinarySearchTree.java
@@ -1,7 +1,10 @@
 package leetcode051_100;
 
+import java.util.Stack;
+
 /**
  * Created by yesongren on 2019/10/11
+ * LeetCode #098- Validate Binary Search Tree
  */
 public class ValidateBinarySearchTree {
     public boolean isValidBST(TreeNode root) {
@@ -13,5 +16,23 @@ public class ValidateBinarySearchTree {
         if (node.val <= min || node.val >= max) return false;   //如果是Integer, 可以用大于或者小于号，但下方递归的边界条件node.val需要加一，减一
         return BSTUtil (node.left, min, node.val) &&
                 BSTUtil (node.right, node.val, max);
+    }
+
+    /************** sol 2 - inorder traversal **************/
+
+    public boolean isValidBST2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while(!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (pre != null && pre.val >= root.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
     }
 }
