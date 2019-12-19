3224a7b514859b5c387a578b01443e9ee0e9c692
yesongren <youshouninn@gmail.com>
2019-12-20 03:12:18 +0800

    accepted solution #875. Koko Eating Bananas

--- src/leetcode851_900/KokoEatingBananas.java
+++ src/leetcode851_900/KokoEatingBananas.java
@@ -3,6 +3,7 @@ package leetcode851_900;
 /**
  * Created by yesongren on 2019/11/27
  * #875. Koko Eating Bananas
+ * Binary Search
  */
 public class KokoEatingBananas {
     public int minEatingSpeed(int[] piles, int H) {
@@ -18,5 +19,26 @@ public class KokoEatingBananas {
             else right = mid;
         }
         return right;
+    }
+
+    public int minEatingSpeed2(int[] piles, int H) {
+        int low = 1;
+        int high = Integer.MAX_VALUE;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (canEat(piles, H, mid)) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+
+    public boolean canEat(int[] piles, int H, int K) {
+        int total = 0;
+        for (int pile : piles) {
+            // total += pile / K;
+            // if (pile % K != 0) total++;
+            total += (pile + K - 1) / K;
+        }
+        return total <= H;
     }
 }
