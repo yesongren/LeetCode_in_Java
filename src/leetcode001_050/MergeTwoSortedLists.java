067319469a473143c7ccd82f7969d6408cd05716
yesongren <youshouninn@gmail.com>
2020-01-26 01:11:34 +0800

    accepted solution #021. Merge Two Sorted Lists

--- src/leetcode001_050/MergeTwoSortedLists.java
+++ src/leetcode001_050/MergeTwoSortedLists.java
@@ -24,4 +24,35 @@ public class MergeTwoSortedLists {
         cur.next = (l1 == null) ? l2 : l1;
         return dummyHead.next;
     }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        /**
+         * 先建立dummy head（root）和 cur，令 cur 指向 root
+         * 当两个指针不同时位空的时候，进入循环：
+         * 比较 val1 和 val2，如果：val1 < val2:
+         *                                     cur.next = l1;
+         *                                     l1 = l1.next;
+         *                                     cur = cur.next;
+         *                                反之：
+         *                                     cur.next = l2;
+         *                                     l2 = l2.next;
+         *                                     cur = cur.next;
+         */
+        ListNode root = new ListNode(0);
+        ListNode cur = root;
+        while(l1 != null || l2 != null) {
+            int val1 = (l1 == null) ? Integer.MAX_VALUE : l1.val;  // 注意处理空指针问题
+            int val2 = (l2 == null) ? Integer.MAX_VALUE : l2.val;
+            if (val1 < val2) {
+                cur.next = l1;
+                l1 = l1.next;
+                cur = cur.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+                cur = cur.next;
+            }
+        }
+        return root.next;
+    }
 }
