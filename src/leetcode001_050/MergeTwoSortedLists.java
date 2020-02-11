6628392eaf844e6c4381b9d1d9e16dd2eff6a4e4
yesongren <youshouninn@gmail.com>
2020-02-11 16:23:16 +0800

    accepted solution #021. Merge Two Sorted Lists

--- src/leetcode001_050/MergeTwoSortedLists.java
+++ src/leetcode001_050/MergeTwoSortedLists.java
@@ -55,4 +55,19 @@ public class MergeTwoSortedLists {
         }
         return root.next;
     }
+
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        /**
+         * recursive
+         */
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists3(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists3(l1, l2.next);
+            return l2;
+        }
+    }
 }
