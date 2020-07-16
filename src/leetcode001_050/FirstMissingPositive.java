a7159130431e461027fefbabf98996315cce1fde
yesongren <youshouninn@gmail.com>
2020-07-17 02:58:57 +0800

    accepted solution #041. First Missing Positive

---
+++ src/leetcode001_050/FirstMissingPositive.java
@@ -0,0 +1,25 @@
+package leetcode001_050;
+
+/**
+ * Created by yesongren on 2020/7/17
+ * 41. First Missing Positive
+ */
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return nums.length + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
