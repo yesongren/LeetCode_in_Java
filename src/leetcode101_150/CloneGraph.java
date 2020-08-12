471c386f82e955dbaae41e857aa60567eaa1b388
yesongren <youshouninn@gmail.com>
2020-08-12 23:42:48 +0800

    AC solution #133. Clone Graph

---
+++ src/leetcode101_150/CloneGraph.java
@@ -0,0 +1,35 @@
+package leetcode101_150;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by yesongren on 2020/8/12
+ * 133. Clone Graph
+ */
+public class CloneGraph {
+    public class UndirectedGraphNode {
+        int val;
+        List<UndirectedGraphNode> neighbors;
+        UndirectedGraphNode(int x) {
+            val = x;
+            neighbors = new ArrayList<>();
+        }
+    }
+
+    private Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+
+    // Time: O(E), Space: O(n)
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        UndirectedGraphNode copy = new UndirectedGraphNode(node.val);
+        map.put(node, copy);
+        for (UndirectedGraphNode n: node.neighbors) {
+            if (map.containsKey(n)) copy.neighbors.add(map.get(n));
+            else copy.neighbors.add(cloneGraph(n));
+        }
+        return copy;
+    }
+}
