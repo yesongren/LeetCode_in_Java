1d02d60ef80f79e637a1afe09aebeac671b6e71d
yesongren <youshouninn@gmail.com>
2020-10-12 17:35:12 +0800

    AC solution #509. Fibonacci Number

--- src/leetcode501_550/FibonacciNumber.java
+++ src/leetcode501_550/FibonacciNumber.java
@@ -26,7 +26,7 @@ public class FibonacciNumber {
         int[] dp = new int[N + 1];
         dp[1] = dp[2] = 1;
         for (int i = 3; i <= N; i++) {
-            dp[i] = dp[i - 1] + dp[i + 1];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[N];
     }
