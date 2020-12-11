568a4580753e3251e733091126acce5e350a477f
yesongren <youshouninn@gmail.com>
2020-12-11 18:53:36 +0800

    AC solution #860. Lemonade Change

---
+++ src/leetcode851_900/LemonadeChange.java
@@ -0,0 +1,33 @@
+package leetcode851_900;
+
+/**
+ * Created by yesongren on 2020/12/11
+ * 860. Lemonade Change
+ */
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five < 1) {
+                    return false;
+                } else {
+                    five--;
+                    ten++;
+                }
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five > 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
