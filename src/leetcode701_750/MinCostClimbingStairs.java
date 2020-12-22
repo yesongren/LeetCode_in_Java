8f6b57518f3fb2c840ce1ade55c7265506e21ddc
yesongren <youshouninn@gmail.com>
2020-12-22 16:03:46 +0800

    AC solution #746. Min Cost Climbing Stairs

---
+++ src/leetcode701_750/MinCostClimbingStairs.java
@@ -0,0 +1,17 @@
+package leetcode701_750;
+
+/**
+ * Created by yesongren on 2020/12/22
+ * 746. Min Cost Climbing Stairs
+ */
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] f = new int[n + 1];
+        f[0] = f[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            f[i] = Math.min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+        }
+        return f[n];
+    }
+}
